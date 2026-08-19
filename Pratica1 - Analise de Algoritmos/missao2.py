a9ef34e1ca4b379c4507be29eaa0c026ef5b02db
Rhydrian <117781018+Rhydrian@users.noreply.github.com>
2026-08-18 23:12:04 -0300

    mudanças poucas

--- Pratica1 - Analise de Algoritmos/missao2.py
+++ Pratica1 - Analise de Algoritmos/missao2.py
@@ -1,6 +1,6 @@
 import time
 
-# Criando uma instancia: vetor de 0 a 9.999.999
+
 print("Gerando vetor na memoria...")
 vetor_gigante = list(range(10000000))
 print("Vetor gerado!\n")
@@ -11,13 +11,13 @@ def busca(lista, alvo):
             return True
     return False
 
-# Cenario A: Buscando o numero 0
+
 inicio = time.time()
 busca(vetor_gigante, 0)
 fim = time.time()
 print(f"Tempo Cenario A (Buscando 0): {fim - inicio:.6f} s")
 
-# Cenario B: Buscando o numero -1
+
 inicio = time.time()
 busca(vetor_gigante, -1)
 fim = time.time()
